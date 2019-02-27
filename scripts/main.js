f35c98877d22e63622a91e5d6a714cbc41c27777
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-02-28 02:17:18 +0700

    Membetulkan logic search sesuai perintah soal

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,24 @@
-function requestJSON(){
+function requestJSON(nama=''){
     fetch("https://swapi.co/api/people/?format=json&page=1")
     .then(resnponse => {return resnponse.json();})
     .then(data =>{
+        var arrayJSON = data.results;
+        console.log(arrayJSON);
         var konten='<tr><th>Nama</th><th>Tinggi</th><th>Berat</th><th>Gender</th>';
-        for(var i = 0; i<data.results.length; i++){
+        for(var i = 0; i<arrayJSON.length; i++){
             console.log(i);
-            console.log(data.results[i].name);
-            konten+='<tr><td>' + data.results[i].name + '</td>' +'<td>' + data.results[i].height + '</td>'
-            +'<td>' + data.results[i].mass + '</td>'
-            +'<td>' + data.results[i].gender + '</td>'+'</tr>';
+            console.log(arrayJSON[i].name);
+            if(nama!==''){
+                if(arrayJSON[i].name.includes(nama)){
+                    konten+='<tr><td>' + arrayJSON[i].name + '</td>' +'<td>' + arrayJSON[i].height + '</td>'
+                    +'<td>' + arrayJSON[i].mass + '</td>'
+                    +'<td>' + arrayJSON[i].gender + '</td>'+'</tr>';
+                }
+            }else{
+                konten+='<tr><td>' + arrayJSON[i].name + '</td>' +'<td>' + arrayJSON[i].height + '</td>'
+            +'<td>' + arrayJSON[i].mass + '</td>'
+            +'<td>' + arrayJSON[i].gender + '</td>'+'</tr>';
+            }
         }
 
         console.log(konten);
@@ -17,31 +27,11 @@ function requestJSON(){
     });
 }
 
-function requestJSONSearch(nama){
-    fetch("https://swapi.co/api/people/?format=json&search=" + nama)
-    .then(resnponse => {return resnponse.json();})
-    .then(data =>{
-        var konten='<tr><th>Nama</th><th>Tinggi</th><th>Berat</th><th>Gender</th>';
-        for(var i = 0; i<data.results.length; i++){
-            console.log(i);
-            console.log(data.results[i].name);
-            konten+='<tr><td>' + data.results[i].name + '</td>' +'<td>' + data.results[i].height + '</td>'
-            +'<td>' + data.results[i].mass + '</td>'
-            +'<td>' + data.results[i].gender + '</td>'+'</tr>';
-        }
-
-        console.log(konten);
-
-        document.querySelector("#data").innerHTML = konten;
-    });
-}
-
-
-requestJSON();
+requestJSON('');
 
 document.querySelector("#cari").addEventListener("click", function(){
     var nama = document.querySelector("#keyNama").value;
-    requestJSONSearch(nama);
+    requestJSON(nama);
     console.log(nama);
 })
 
